aa87d4d9eeaab4983996db2e3459761e9e060536
Suguru Odai <ourqwers@gmail.com>
2015-02-11 09:06:28 +0900

    Use Heroku enviroment variable to the firebase app name

--- config/initializers/word_basket.rb
+++ config/initializers/word_basket.rb
@@ -4,5 +4,5 @@ require 'moji'
 
 WordBasket.configure do |config|
   config.database_adapter = 'firebase'
-  config.database_options = { app_name: 'odailly-wordbaskets' }
+  config.database_options = { app_name: ENV['FIREBASE_APP'] }
 end
